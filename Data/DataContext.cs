68e694ab40ed0de9705be22154a8c9dbfe8ba15e
and2684 <aaaa6226@gmail.com>
2022-07-29 08:58:13 +0500

    Добавьте файлы проекта.

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DogeFriendsAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace DogeFriendsAPI.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string SecondaryName { get; set; } = string.Empty;
+        public bool IsCompany { get; set; }
+        public byte[]? ProfilePhoto { get; set; }
+    }
+}
