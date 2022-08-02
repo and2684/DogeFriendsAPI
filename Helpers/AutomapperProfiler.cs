29d7db890ec33cb435a6613fcfa4f7b633715c49
and2684 <aaaa6226@gmail.com>
2022-08-02 16:04:04 +0500

    Removed some random comment

--- Helpers/AutomapperProfiler.cs
+++ Helpers/AutomapperProfiler.cs
@@ -6,7 +6,7 @@ namespace API.Helpers
     {
         public AutomapperProfiles()
         {
-            CreateMap<User, PersonDto>() // Map from User to PersonDto
+            CreateMap<User, PersonDto>()
                 .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}".Trim())); // Собираем полное имя (Имя Фамилия)
         }
     }
