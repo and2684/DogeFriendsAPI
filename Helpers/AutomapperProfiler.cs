72a0d8246dffe2696c5a384186307caed33ba359
and2684 <aaaa6226@gmail.com>
2022-08-12 15:37:31 +0500

    Some automapper profile changes

--- Helpers/AutomapperProfiler.cs
+++ Helpers/AutomapperProfiler.cs
@@ -1,4 +1,3 @@
-using System.Security.Cryptography;
 using DogeFriendsAPI.Dto;
 
 namespace API.Helpers
@@ -10,13 +9,13 @@ namespace API.Helpers
             CreateMap<User, PersonDto>()
                 .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}".Trim())); // Собираем полное имя (Имя Фамилия)
 
-            CreateMap<User, UserShowDto>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Username))
-                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
-                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
-                .ForMember(dest => dest.IsCompany, opt => opt.MapFrom(src => src.IsCompany))
-                .ForMember(dest => dest.ProfilePhoto, opt => opt.MapFrom(src => src.ProfilePhoto));
+            CreateMap<User, UserShowDto>();
+                // .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                // .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Username))
+                // .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
+                // .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
+                // .ForMember(dest => dest.IsCompany, opt => opt.MapFrom(src => src.IsCompany))
+                // .ForMember(dest => dest.ProfilePhoto, opt => opt.MapFrom(src => src.ProfilePhoto));
 
             // Не вышло, маппер не видит hmac. К тому же, логика в маппере - это неправильно.
             // using var hmac = new HMACSHA512();
