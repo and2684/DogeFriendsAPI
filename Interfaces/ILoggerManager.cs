745c7aeb00be57de5f6426f2a6cfc4b1a862cef5
and2684 <aaaa6226@gmail.com>
2022-08-17 14:59:49 +0500

    Add logging to Exception Middleware

--- Interfaces/ILoggerManager.cs
+++ Interfaces/ILoggerManager.cs
@@ -3,5 +3,5 @@ public interface ILoggerManager
     void LogInfo(string message);
     void LogWarn(string message);
     void LogDebug(string message);
-    void LogError(string message);
+    void LogError(Exception ex, string message);
 }
--- Middleware/ExceptionMiddleware.cs
+++ Middleware/ExceptionMiddleware.cs
@@ -25,7 +25,7 @@ namespace API.Middleware
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex.Message);
+                _logger.LogError(ex, ex.Message);
 
                 context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
